900f5d5b77289d14ab19ff0a73f839cdcbb4938d
Andriy Malyon <a.r.malyon@gmail.com>
2020-10-26 20:50:03 +0200

    UI Tasks: ToDo list second aproach - @wire works, adding, editing and deleting of ToDos works

--- src/lwc/toDoCard/toDoCard.js
+++ src/lwc/toDoCard/toDoCard.js
@@ -1,65 +1,30 @@
-import {LightningElement, api, track} from 'lwc';
-import {deleteRecord} from 'lightning/uiRecordApi';
-import {ShowToastEvent} from 'lightning/platformShowToastEvent';
-import getToDoById from '@salesforce/apex/ToDoController.getToDoById'
+import {LightningElement, api} from 'lwc';
 
 export default class ToDoCard extends LightningElement {
-    @api recordId;
-    @track name;
-    @track description;
-    @track category;
-    @track startDate;
-    @track deadline;
-    @track status;
+  @api todo;
 
-    @track isOpenEditModal = false;
+  isOpenEditModal = false;
+  showExpandBody = false;
 
-    connectedCallback() {
-        this.loadRecord();
-    }
+  handleClickOnDelete() {
+    console.log('log from card: ' + this.todo.Id)
+    const deleteEvent = new CustomEvent('deletetodo', {
+      detail: this.todo.Id
+    });
+    this.dispatchEvent(deleteEvent);
+  }
 
+  handleClickOnEdit() {
+    this.isOpenEditModal = true;
+  }
 
-    handleClickOnDelete() {
-        deleteRecord(this.recordId).then(() => {
-                const event = new ShowToastEvent({
-                    title: 'Deleted',
-                    variant: 'success',
-                    mode: 'dismissable'
-                });
-                this.dispatchEvent(event);
-            }
-        ).catch(error => {
-            console.log(error);
-            const event = new ShowToastEvent({
-                title: 'Error occurred',
-                variant: 'error',
-                mode: 'dismissable'
-            });
-            this.dispatchEvent(event);
-        });
-        this.dispatchEvent(new CustomEvent('recordscountupdated'));
-    }
+  closeEditModal() {
+    this.isOpenEditModal = false;
+    this.dispatchEvent(new CustomEvent('refreshlist'));
+  }
 
-    handleClickOnEdit() {
-        this.isOpenEditModal = true;
-    }
-
-    closeEditModal() {
-        this.isOpenEditModal = false;
-
-    }
-
-
-    loadRecord() {
-        getToDoById({recordId: this.recordId}).then(result => {
-                this.name = result.Name;
-                this.description;
-                this.category;
-                this.startDate;
-                this.deadline;
-                this.status;
-            }
-        )
-    }
+  showExpandOrCollapsed() {
+    this.showExpandBody = !this.showExpandBody;
+  }
 
 }
--- src/lwc/toDosContainer/toDosContainer.js
+++ src/lwc/toDosContainer/toDosContainer.js
@@ -1,33 +1,71 @@
-import {LightningElement, track} from 'lwc';
+import {LightningElement, wire} from 'lwc';
+
 import getToDoList from '@salesforce/apex/ToDoController.getToDoList'
 import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+import {deleteRecord} from "lightning/uiRecordApi";
+import {refreshApex} from "@salesforce/apex";
+import {reduceErrors} from 'c/ldsUtils';
 
 export default class ToDosContainer extends LightningElement {
 
+  todos;
+  todo = {"Id" : null};
+  wiredTodosResult;
+  isOpenCreateModal = false;
 
-    @track records;
-
-    connectedCallback() {
-        this.loadRecords();
+  @wire(getToDoList)
+  loadRecords(result) {
+    this.wiredTodosResult = result;
+    console.log('load records invoked');
+    if (result.data) {
+      this.todos = result.data;
+      console.log(this.todos);
+      this.error = undefined;
+    } else if (result.error) {
+      this.error = result.error;
+      this.todos = undefined;
     }
+  }
 
+  connectedCallback() {
+    this.refreshList();
+  }
 
-    loadRecords() {
-        console.log('load records invoked');
-        getToDoList().then(result => {
-            this.records = result;
-            console.log(this.records);
-        }).catch(error => {
-            console.log(error);
-            const event = new ShowToastEvent({
-                title: 'Error occurred',
-                variant: 'error',
-                message: error,
-                mode: 'dismissable'
-            });
-            this.dispatchEvent(event);
-        })
-    }
+  handleDeleteTodo(event) {
+    const todoId = event.detail;
+    console.log('Log from delete handle: ' + todoId)
+    deleteRecord(todoId)
+    .then(() => {
+      this.dispatchEvent(
+          new ShowToastEvent({
+            title: 'Success',
+            message: 'Todo deleted',
+            variant: 'success'
+          })
+      );
+      return refreshApex(this.wiredTodosResult);
+    })
+    .catch((error) => {
+      this.dispatchEvent(
+          new ShowToastEvent({
+            title: 'Error deleting todo',
+            message: reduceErrors(error).join(', '),
+            variant: 'error'
+          })
+      );
+    });
+  }
+  refreshList(){
+    return refreshApex(this.wiredTodosResult);
+  }
 
+  handleClickCreateTodo() {
+    this.isOpenCreateModal = true;
+    console.log('isopen= ' + this.isOpenCreateModal);
+  }
 
+  closeCreateModal() {
+    this.isOpenCreateModal = false;
+    return this.refreshList();
+  }
 }
